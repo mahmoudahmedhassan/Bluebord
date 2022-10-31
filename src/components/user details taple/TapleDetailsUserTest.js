70117af0d46c7129244bd4cf8379a49147540cec
Mahmoud Ahmed <hekou77@gmail.com>
2022-10-31 19:59:02 +0200

    add valdation in taple in put

--- src/components/user details taple/TapleDetailsUserTest.js
+++ src/components/user details taple/TapleDetailsUserTest.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 import BootstrapTable from 'react-bootstrap-table-next';
-import cellEditFactory, {type} from 'react-bootstrap-table2-editor';
+import cellEditFactory, { type } from 'react-bootstrap-table2-editor';
 
 
 function TapleDetailsUserTest() {
@@ -10,67 +10,67 @@ function TapleDetailsUserTest() {
     console.log(userTapleData)
 
     const columns = [
- 
-    {
-        text: 'T201',
-        dataField: "t201",
-        validator: (newValue, row, column) => {
-            if (isNaN(newValue)) {
-              return {
-                valid: false,
-                message: 'Price should be numeric'
-              };
+
+        {
+            text: 'T201',
+            dataField: "t201",
+            validator: (newValue, row, column) => {
+                if (isNaN(newValue)) {
+                    return {
+                        valid: false,
+                        message: 'Price should be numeric'
+                    };
+                }
+                return true;
+
             }
+        },
+        {
+            text: 'T202',
+            dataField: "t202",
+        },
+        {
+            text: 'T203',
+            dataField: "t203",
+            validator: (newValue, row, column) => {
+                if (isNaN(newValue)) {
+                    return {
+                        valid: false,
+                        message: 'Price should be numeric'
+                    };
+                }
+                return true;
+
             }
-    },
-    {
-        text: 'T202',
-        dataField: "t202",
-        validator: (newValue, row, column) => {
-            if (isNaN(newValue)) {
-              return {
-                valid: false,
-                message: 'Price should be numeric'
-              };
+        },
+        {
+            text: 'T204',
+            dataField: "t204",
+            validator: (newValue, row, column) => {
+                if (isNaN(newValue)) {
+                    return {
+                        valid: false,
+                        message: 'Price should be numeric'
+                    };
+                }
+                return true;
+
             }
+        },
+        {
+            text: 'T205',
+            dataField: "t205",
+            validator: (newValue, row, column) => {
+                if (isNaN(newValue)) {
+                    return {
+                        valid: false,
+                        message: 'Price should be numeric'
+                    };
+                }
+                return true;
             }
-    },
-    {
-        text: 'T203',
-        dataField: "t203",
-        validator: (newValue, row, column) => {
-            if (isNaN(newValue)) {
-              return {
-                valid: false,
-                message: 'Price should be numeric'
-              };
-            }
-            }
-    },
-    {
-        text: 'T204',
-        dataField: "t204",
-        validator: (newValue, row, column) => {
-            if (isNaN(newValue)) {
-              return {
-                valid: false,
-                message: 'Price should be numeric'
-              };
-            }
-            }
-    },
-    {
-        text: 'T205',
-        dataField: "t205",
-        validator: (newValue, row, column) => {
-            if (isNaN(newValue)) {
-              return {
-                valid: false,
-                message: 'Price should be numeric'
-              };
-            }
-            }
-    },
+        },
+
     ];
     return (
         <div>
@@ -80,7 +80,7 @@ function TapleDetailsUserTest() {
                 columns={columns}
                 cellEdit={cellEditFactory({
                     mode: 'click',
-                 })}
+                })}
             />
 
         </div>
